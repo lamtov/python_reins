57e42f372bd0dc9607508ed5dd2e1c7c8a24886c
To Van Lam <tovanlam20132223@gmail.com>
2021-02-08 18:25:20 +0700

    update

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+## install chrome for ubuntu
+```
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
+```
 ## update chrome-driver for ubuntu
 ```
 sudo apt-get update -y
--- bot.py
+++ bot.py
@@ -29,10 +29,10 @@ def init_driver_window():
 
 def init_driver():
     chrome_options = Options()
-    # chrome_options.add_argument("ignore-certificate-errors")
+    chrome_options.add_argument("ignore-certificate-errors")
     # chrome_options.add_argument('--headless')
     # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     # chrome_options.add_argument("--incognito")
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("user-data-dir=selenium")
