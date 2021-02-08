094b9ce96d2694f59f4832135c09331879058dcc
To Van Lam <tovanlam20132223@gmail.com>
2021-02-08 17:36:19 +0700

    first commit

---
+++ bot.py
@@ -0,0 +1,210 @@
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import traceback
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
+import io, json,os
+import time
+soup = BeautifulSoup("")
+
+def init_driver_ubuntu():
+    chrome_options = Options()
+    # chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--no-sandbox')
+    # chrome_options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    return driver
+
+
+def init_driver():
+    chrome_options = Options()
+
+    # chrome_options.add_argument("ignore-certificate-errors")
+    # chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--no-sandbox')
+    # chrome_options.add_argument('--disable-dev-shm-usage')
+    # chrome_options.add_argument("--incognito")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("user-data-dir=selenium")
+    chrome_options.add_argument("--window-size=1920x1080")
+    driver = webdriver.Chrome(chrome_options=chrome_options,
+                              executable_path="C:/Users/Lam/Documents/Selenium/chromedriver_win32/chromedriver.exe")
+    return driver
+
+
+
+
+class Bot():
+    def __init__(self, username, password, link_login, link_search,output_folder):
+        try:
+            self.browser = init_driver()
+            self.username = username
+            self.password = password
+            self.status = 'init'
+            self.number_post = 0
+            self.time_block = 0
+            self.start_block = None
+            self.link_login=link_login
+            self.link_search = link_search
+            self.output_folder= output_folder
+        except Exception as e:
+            self.status = str(traceback.format_exc())
+
+    def login(self):
+        try:
+            self.browser.get(self.link_login)
+            usernameInput= WebDriverWait(self.browser, 15).until(ec.visibility_of_element_located((By.XPATH, '''//input[@id='__BVID__13']
+''')))
+            passwordInput = WebDriverWait(self.browser, 15).until(ec.visibility_of_element_located((By.XPATH, '''//input[@id='__BVID__16']
+''')))
+            usernameInput.send_keys(self.username)
+            passwordInput.send_keys(self.password)
+            self.status = 'OK'
+        except Exception as e:
+            print(e)
+            self.status = str('LOGIN_FAILED')
+    def crawl_data_page(self,soup,search_text,count_page):
+        try:
+            name_to_style = {
+                "No.": 'grid-row: 1 / span 2; grid-column-start: 1;',
+                "Property No": "grid-row-start: 1; grid-column: 2 / span 3;",
+                "Property item": "grid-row-start: 1; grid-column: 5 / span 5;",
+                "Occupied area": "grid-row-start: 1; grid-column: 10 / span 3;",
+                "location": "grid-row-start: 1; grid-column: 13 / span 12;",
+                "Transaction mode": "grid-row-start: 2; grid-column: 2 / span 3;",
+                "price": "grid-row-start: 2; grid-column: 5 / span 3;",
+                "Area of use": "grid-row-start: 2; grid-column: 8 / span 2;",
+                "mxm Unit price": "grid-row-start: 2; grid-column: 10 / span 3;",
+                "Building name": "grid-row-start: 2; grid-column: 13 / span 6;",
+                "Location floor": "grid-row-start: 2; grid-column: 19 / span 4;",
+                "Floor plan": "grid-row-start: 2; grid-column: 23 / span 2;",
+                "Trading conditions": "grid-row-start: 3; grid-column: 2 / span 3;",
+                "Management fee": "grid-row-start: 3; grid-column: 5 / span 3;",
+                "Unit price per tsubo": "grid-row-start: 3; grid-column: 10 / span 3;",
+                "Stations along the line": "grid-row-start: 3; grid-column: 13 / span 6;",
+                "traffic": "grid-row-start: 3; grid-column: 19 / span 6;",
+                "Trade name": "grid-row-start: 4; grid-column: 13 / span 12;",
+                "Date of construction": "grid-row-start: 5; grid-column: 5 / span 4;",
+                "phone number": "grid-row-start: 5; grid-column: 13 / span 6;"
+            }
+
+            body_row = soup.find_all('div', class_='p-table-body-row')
+            list_data_row = []
+            for row in body_row:
+
+                data_row = {}
+                for style in name_to_style:
+                    try:
+                        list_item = row.find('div', attrs={'style': name_to_style[style]})
+                        if list_item is not None:
+                            data_row[style] = list_item.string
+                    except Exception as e:
+                        print(e)
+                list_data_row.append(data_row)
+
+            with io.open(self.output_folder+'/'+ search_text+'/'+ search_text+'_'+'page_'+str(count_page)+ '.txt', 'w',
+                         encoding='utf-8') as f:
+                f.write(json.dumps(list_data_row, ensure_ascii=False))
+        except Exception as e:
+            print(e)
+            self.status = str('run_to_page_file failed')
+
+
+    def run_to_page_file(self,search_text):
+        try:
+            self.browser.get(self.link_search)
+            property_type_1_select_box =Select(WebDriverWait(self.browser, 15).until(ec.visibility_of_element_located((By.XPATH, '''//select[@id='__BVID__41']'''))))
+            property_type_1_select_box.select_by_value('03')
+
+            prefecturesInput = WebDriverWait(self.browser, 15).until(ec.visibility_of_element_located((By.XPATH, '''//input[@id='__BVID__94']''')))
+            prefecturesInput.send_keys(search_text)
+        except Exception as e:
+            print(e)
+            self.status = str('run_to_page_file failed')
+
+        try:
+            btnSearch = WebDriverWait(self.browser, 15).until(ec.visibility_of_element_located((By.CSS_SELECTOR, ''' div.col-sm-3.col-4:nth-child(4) > button.btn.p-button.btn-primary.btn-block.px-0''')))
+            btnSearch.send_keys(Keys.COMMAND, Keys.ENTER)
+        except Exception as e:
+            print(e)
+            self.status = str('run_to_page_file failed')
+
+        OKclick=False
+        try:
+            btnOK = WebDriverWait(self.browser, 15).until(ec.visibility_of_element_located(
+                (By.XPATH, '''//button[@class='btn btn-primary'][.='OK']''')))
+            btnOK.send_keys(Keys.COMMAND, Keys.ENTER)
+            OKclick= True
+        except Exception as e:
+            print(e)
+            self.status = str('run_to_page_file failed')
+
+        if OKclick==False:
+            try:
+                btnOK = WebDriverWait(self.browser, 10).until(ec.visibility_of_element_located(
+                    (By.XPATH, '''//font[text()='OK']/ancestor::font/ancestor::button[@class='btn btn-primary']''')))
+                btnOK.send_keys(Keys.COMMAND, Keys.ENTER)
+            except Exception as e:
+                print(e)
+                self.status = str('run_to_page_file failed')
+        crawl_start=True
+        count_page=1
+        while crawl_start:
+            try:
+
+                # print('''//li[@class='page-item active']//button[contains(text(), "{0}") and @class='page-link']'''.format(str(count_page)))
+                btn_active=WebDriverWait(self.browser, 15).until(ec.visibility_of_element_located(
+                    (By.XPATH, '''//div[@class='p-table-body-row']//div[contains(text(), "{0}") and @class='p-table-body-item large']'''.format(str((count_page-1)*50+1)))))
+                # print(btn_active)
+
+                page_source = self.browser.page_source
+                soup = BeautifulSoup(page_source, 'lxml')
+                self.crawl_data_page(soup,search_text,count_page)
+                count_page=count_page+1
+                btnNextPage = WebDriverWait(self.browser, 15).until(ec.visibility_of_element_located(
+                    (By.XPATH, '''//button[@aria-label='Go to next page'][@class='page-link']''')))
+                btnNextPage.send_keys(Keys.COMMAND, Keys.ENTER)
+                # time.sleep(1)
+            except Exception as e:
+                print(e)
+                self.status = str('run_to_page_file failed')
+                crawl_start=False
+
+        print("END")
+    def check_login(self):
+        try:
+            self.browser.get(self.link_search)
+            try:
+                btnSearch = WebDriverWait(self.browser, 15).until(ec.visibility_of_element_located((By.CSS_SELECTOR, ''' div.col-sm-3.col-4:nth-child(4) > button.btn.p-button.btn-primary.btn-block.px-0''')))
+                return True
+            except:
+                print("not OK")
+                return False
+        except Exception as e:
+            print(e)
+            return  False
+
+    def close(self):
+        self.browser.close()
+
+    def to_json(self):
+        return {'username': self.username,
+                'password': self.password, 'status': self.status, 'number_post': self.number_post,
+                'time_block': self.time_block
+                }
+
+
+import os.path
+
+# if __name__ == "__main__":
+#     username = "lane287279067"
+#     password = "To01111995"
+#     t = Bot(username, password)
+#     t.login()
+
---
+++ main.py
@@ -0,0 +1,169 @@
+import cv2
+import os
+from tkinter import *
+from tkinter import filedialog as fd
+import tkinter as tk
+from os import listdir
+from os.path import isfile, join
+import os
+import cv2
+import numpy as np
+from PIL import ImageTk, ImageFont, ImageDraw, Image
+from threading import Lock
+from concurrent.futures import ThreadPoolExecutor
+import logging
+cur_dir= os.getcwd()
+folder_dataset = cur_dir+ '/dataset/'
+from tkinter import messagebox
+
+from bot import Bot
+class MyWindow:
+    def __init__(self, win):
+
+        self.file_input = cur_dir+ '/dataset/input.txt'
+        self.folder_output = cur_dir+ '/dataset/'+ 'output/'
+
+        self.lb_note = Label(win, text='NOTE:  OUTPUT FOLDER!', bg="white", fg="red", font=("Arial", 11))
+
+        self.lb_file_input= Label(win, text='Folder Dataset (default: ' + self.file_input + ')', bg="black", fg="white",
+                          font=("Arial", 10))
+        self.lb_text_file_input= Label(win, text='Dataset: (default: ' + self.file_input + ')', font=("Arial", 10))
+        self.bt_file_input = Button(text='Select input.txt', bg="gray", fg="blue", command=self.callback_file_input)
+
+        self.lb_folder_output = Label(win, text='Folder output (default: ' + self.folder_output + ')', bg="black", fg="white",font=("Arial", 10))
+        self.lb_text_folder_output= Label(win, text='Output: (default: ' + self.folder_output + ')', font=("Arial", 10))
+
+        self.bt_folder_output = Button(text='Select folder output', bg="gray", fg="blue",
+                                        command=self.callback_folder_output)
+        self.lb_note.place(x=0, y=10)
+        self.lb_file_input.place(x=100, y=50)
+        self.bt_file_input.place(x=800, y=50)
+        self.lb_text_file_input.place(x=130, y=80)
+        self.lb_folder_output.place(x=100, y=120)
+        self.bt_folder_output.place(x=800, y=120)
+        self.lb_text_folder_output.place(x=130, y=150)
+
+        self.user_login='123023951000'
+        self.password_login='iejuc6'
+        self.number_thread=1
+        self.link_login='https://system.reins.jp/login/main/KG/GKG001200'
+        self.link_search='https://system.reins.jp/main/BK/GBK001200'
+
+        self.lb_user_login = Label(win, text='user login: ')
+        self.et_user_login = Entry(win, bd=2, foreground="red")
+        self.lb_password_login = Label(win, text='password : ')
+        self.et_password_login = Entry(win, bd=2, foreground="red")
+        self.lb_number_thread = Label(win, text='number threads: ')
+        self.et_number_thread = Entry(win, bd=2, foreground="red")
+        self.lb_link_login = Label(win, text='link login: ')
+        self.et_link_login = Entry(win, bd=2, foreground="red")
+        self.lb_link_search = Label(win, text='link search: ')
+        self.et_link_search = Entry(win, bd=2, foreground="red")
+        self.et_user_login.insert(END, str(self.user_login))
+        self.et_password_login.insert(END, str(self.password_login))
+
+        self.et_number_thread.insert(END, str(self.number_thread))
+        self.et_link_login.insert(END, str(self.link_login))
+        self.et_link_search.insert(END, str(self.link_search))
+
+
+
+
+        self.lb_user_login.place(x=130, y=190)
+        self.et_user_login.place(x=305, y=190)
+        self.lb_password_login.place(x=480, y=190)
+        self.et_password_login.place(x=655, y=190)
+
+        self.lb_number_thread.place(x=130, y=230)
+        self.et_number_thread.place(x=305, y=230)
+        self.lb_link_login.place(x=480, y=230)
+        self.et_link_login.place(x=655, y=230)
+        self.lb_link_search.place(x=480, y=270)
+        self.et_link_search.place(x=655, y=270)
+        self.b1 = Button(win, text='LOGIN', bg="green", fg="white", command=self.login)
+        self.b2 = Button(win, text='RUN', bg="red", fg="white", command=self.run)
+        self.b1.place(x=370, y=310)
+        self.b2.place(x=440, y=310)
+        self.list_bot=[]
+        self.bot=None
+
+    def login(self):
+        if not os.path.exists(self.folder_output):
+            os.makedirs(self.folder_output)
+        self.user_login=str(self.et_user_login.get())
+        self.password_login = str(self.et_password_login.get())
+        self.link_login = str(self.et_link_login.get())
+        self.link_search = str(self.et_link_search.get())
+
+        if self.bot is None:
+            self.bot = Bot(username=self.user_login, password=self.password_login, link_login=self.link_login,
+                      link_search=self.link_search,output_folder=self.folder_output)
+            self.list_bot.append(self.bot)
+
+        self.bot.login()
+
+
+    def run(self):
+        try:
+            if not os.path.exists(self.folder_output):
+                os.makedirs(self.folder_output)
+            if self.bot is None:
+                self.user_login = str(self.et_user_login.get())
+                self.password_login = str(self.et_password_login.get())
+                self.link_login = str(self.et_link_login.get())
+                self.link_search = str(self.et_link_search.get())
+                self.bot = Bot(username=self.user_login, password=self.password_login, link_login=self.link_login,
+                               link_search=self.link_search,output_folder=self.folder_output)
+                self.list_bot.append(self.bot)
+
+            if self.bot.check_login() is False:
+                messagebox.showerror(title="ERROR", message="PLEASE LOGIN BEFORE RUN!")
+            else:
+                txt_file = open(self.file_input, encoding="utf8")
+                Lines = txt_file.readlines()
+                for line in Lines:
+                    search_text = line.replace("\n","")
+                    if not os.path.exists(self.folder_output+'/'+search_text):
+                        os.makedirs(self.folder_output+'/'+search_text)
+                    self.bot.run_to_page_file(search_text)
+            messagebox.showinfo(title="DONE", message="DONE ALL!")
+
+        except Exception as e:
+            logging.debug(str(e))
+            logging.debug("ERROR run_in_thread")
+
+    # def run(self):
+    #     executor = ThreadPoolExecutor(1)
+    #     executor.submit(self.run_in_thread())
+
+    def callback_file_input(self):
+        try:
+            self.file_input = fd.askopenfilename(filetypes=[("TXT files", "*.txt")])
+            self.lb_text_file_input.configure(text=str('Dataset: ' + self.file_input ))
+        except Exception as e:
+            logging.debug(str(e))
+            logging.debug("ERROR callback_folder_dataset")
+
+    def callback_folder_output(self):
+        try:
+            self.folder_output = fd.askdirectory()
+            self.lb_text_folder_output.configure(text=str('Output: ' + self.folder_output ))
+        except Exception as e:
+            logging.debug(str(e))
+            logging.debug("ERROR callback_folder_output")
+
+
+window = Tk()
+try:
+    imagebg = Image.open(cur_dir + '/dataset/back_ground.jpg')
+    imagebg.resize((1300, 450))
+    photo_image = ImageTk.PhotoImage(imagebg)
+    label = tk.Label(window, image=photo_image)
+    label.pack()
+except Exception as e:
+    logging.debug(str(e))
+mywin = MyWindow(window)
+
+window.title('CONVERT IMAGE TO VIDEO')
+window.geometry("1300x600+10+10")
+window.mainloop()
